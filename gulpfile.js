86ef78d34c3e46126f2e504e9715e446337b5e91
Schuurmans <rosa@wetteloos.nl>
2017-03-20 13:46:58 +0100

    add/remove modules, module detail, son data

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,17 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var nodemon     = require('gulp-nodemon');
 var	sass        = require('gulp-sass');
-var concat     = require('gulp-concat');
+var concat      = require('gulp-concat');
+var jsonlint    = require('gulp-json-lint');
 
-gulp.task('default', ['browser-sync', 'build-lib'], function () {
-	gulp.watch('public/sass/**/*.scss', ['build-css'])
-	gulp.watch('public/javascript/**/*.js', ['build-js'])
-});
+gulp.task('default', ['browser-sync', 'build', 'watch']);
+
+gulp.task('build', ['build-lib', 'build-js', 'build-css', 'jsonlint'])
+
+gulp.task('watch', function () {
+	gulp.watch('src/sass/*.scss', ['build-css'])
+	gulp.watch('src/javascript/*.js', ['build-js'])
+})
 
 gulp.task('browser-sync', ['nodemon'], function() {
 	browserSync.init(null, {
@@ -19,25 +24,32 @@ gulp.task('browser-sync', ['nodemon'], function() {
 	});
 });
 
+gulp.task('jsonlint', function(){
+    return gulp.src('src/data/*.json')
+        .pipe(jsonlint())
+        .pipe(jsonlint.report('verbose'))
+        .pipe(gulp.dest('public/data'))
+});
+
 gulp.task('build-css', function() {
-  return gulp.src('public/sass/style.scss')
+  return gulp.src('src/sass/style.scss')
     .pipe(sass())
-    .pipe(gulp.dest('public'))
-    
+    .pipe(gulp.dest('public'))    
 });
 
 gulp.task('build-js', function () {
-	return gulp.src('public/javascript/*.js')
+	console.log('ee');
+	return gulp.src('src/javascript/*.js')
 		.pipe(concat('client.js'))
 		.pipe(gulp.dest('public'))
 })
 gulp.task('build-lib', function () {
-	return gulp.src('public/javascript/lib/*.js')
+	return gulp.src('src/javascript/lib/*.js')
 		.pipe(concat('lib.js'))
 		.pipe(gulp.dest('public'))	
 })
+
 gulp.task('nodemon', function (cb) {
-	
 	var started = false;
 	
 	return nodemon({
--- routes/play.js
+++ routes/play.js
@@ -4,39 +4,104 @@ var fs         = require('fs');
 
 
 var combineGrid = function (grid, activeModules) {
-	// console.log(grid, user);
 	for(var i in activeModules) {
 		grid.rows[activeModules[i].row].items[activeModules[i].col] = activeModules[i]
-
 	}
 	return grid
 }
-router.get('/', function(req, res, next) { 
-	
-	var data = new Object();
-	data.user = JSON.parse(fs.readFileSync('data/user.json', 'utf8'));
-	data.grid = combineGrid(JSON.parse(fs.readFileSync('data/grid.json', 'utf8')), data.user.activeModules);
-	
-	res.render('play/main', data);
-	
-});
-router.post('/remove', function (req, res, next) {
-	var data  = JSON.parse(fs.readFileSync('data/user.json', 'utf8'));
-	
-	var activeModules = data.activeModules;
+var removeObj = function (obj, prop, target) {
 	var index;
-	console.log(req.body.moduleId);
-	for(var i in activeModules) {
-		if(activeModules[i].id == req.body.moduleId) {
+
+	for(var i in obj) {
+		if(obj[i][prop] == target) {
 			index = i;
 		}
 	}
-	activeModules.splice(index, 1);
-	console.log(activeModules);
-	data.activeModules = activeModules;
+	obj.splice(index, 1);
+	return obj
+}
+var newModule = function (req, data) {
+	console.log(req, data);
+	var newItem = {};
+	for(var i in data ){
+		for(var y in data[i].modules) {
+			console.log('hier',req.gridPlace.col);
+			if(data[i].modules[y].id == parseInt(req.moduleId)) {
+				
+				newItem          = data[i].modules[y];
+				newItem.category = data[i].category;
+				newItem.col      = req.gridPlace.col;
+				newItem.row      = req.gridPlace.row;
+			}
+		}
+	}
+	console.log('newItem' , newItem);
+	return newItem;
+}
+var getModule = function (id){ 
+	var data = JSON.parse(fs.readFileSync('public/data/availableModules.json', 'utf8')).categories;
+	var module = {};
+	for(var i in data) {
+		for(var y in data[i].modules) {
+			
+			if(data[i].modules[y].id == id) {
+				module = data[i].modules[y];
+				break;
+			}
+		}
+	}
+	return module;
+}
+
+router.get('/', function(req, res, next) { 
 	
-	fs.writeFileSync('data/user.json', JSON.stringify(data));
-	res.redirect('/play')
+	var data = new Object();
+	data.user = JSON.parse(fs.readFileSync('public/data/user.json', 'utf8'));
+	
+	data.grid = combineGrid(JSON.parse(fs.readFileSync('public/data/grid.json', 'utf8')), data.user.activeModules);
+	console.log(data.grid.rows[0]);
+	res.render('play/overview', data);
+	
+});
+router.get('/module/:id', function (req, res, next) {
+	console.log(req.params.id);
+	var data = getModule(req.params.id);
+	res.render('play/module', data);
+})
+router.get('/list/:place', function (req, res, next ) {
+	console.log(req.params.place);
+	
+	
+
+	var data = JSON.parse(fs.readFileSync('public/data/availableModules.json', 'utf8'));
+
+	res.render('play/list', data)
+});
+router.post('/add', function (req, res, next) {
+	var user    = JSON.parse(fs.readFileSync('public/data/user.json', 'utf8'));
+	
+	
+	var newItem = newModule(req.body, JSON.parse(fs.readFileSync('public/data/availableModules.json', 'utf8')).categories);
+	user.activeModules.push(newItem);
+	fs.writeFileSync('public/data/user.json', JSON.stringify(user, null, 2));
+	res.send({
+		redirectTo: '/play'
+	})
+	
+	
+
+})
+router.post('/remove', function (req, res, next) {
+	console.log('post /remove');
+	var data  = JSON.parse(fs.readFileSync('public/data/user.json', 'utf8'));
+	
+	data.activeModules = removeObj(data.activeModules, 'id', req.body.moduleId);
+
+	fs.writeFileSync('public/data/user.json', JSON.stringify(data));
+	res.send({
+		redirectTo: '/play'
+	})
+	
 
 
 })
