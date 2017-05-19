7201bc25a316882980ca9c8750c72e18e090b32b
Schuurmans <rosa@wetteloos.nl>
2017-05-19 16:42:14 +0200

    init new_layout

--- src/javascript/animationController.js
+++ src/javascript/animationController.js
@@ -14,7 +14,7 @@ var animate = {
 		}
 		var createShape = function () {
 			var color        = colors[Math.floor(Math.random()*colors.length)];
-			var shapes       = ['<svg width="34px" height="31px" viewBox="0 0 34 31" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">    <!-- Generator: Sketch 43.2 (39069) - http://www.bohemiancoding.com/sketch -->    <desc>Created with Sketch.</desc>    <defs></defs>    <g id="design-v2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">        <g id="iPhone-7-Copy-68" transform="translate(-172.000000, -33.000000)" fill=" ' +color+ ' ">            <g id="bg">                <g id="animated" transform="translate(10.000000, 33.000000)">                    <polygon id="Polygon" points="179 0 195.167961 11.7467111 188.992349 30.7532889 169.007651 30.7532889 162.832039 11.7467111"></polygon>                </g></g></g></g></svg>', '<svg width="62px" height="64px" viewBox="0 0 62 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><!-- Generator: Sketch 43.2 (39069) - http://www.bohemiancoding.com/sketch --><desc>Created with Sketch.</desc><defs></defs><g id="design-v2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="iPhone-7-Copy-68" transform="translate(-34.000000, -162.000000)" stroke-width="8" stroke="'+color+'"><g id="bg"><g id="animated" transform="translate(10.000000, 33.000000)"><path d="M43,134.408029 L69.6816418,176 L16.3183582,176 L43,134.408029 Z" id="Triangle-2" transform="translate(43.000000, 153.500000) rotate(-27.000000) translate(-43.000000, -153.500000) "></path></g></g></g></g></svg>', '<svg width="66px" height="66px" viewBox="0 0 66 66" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><!-- Generator: Sketch 43.2 (39069) - http://www.bohemiancoding.com/sketch --><desc>Created with Sketch.</desc><defs></defs><g id="design-v2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="iPhone-7-Copy-68" transform="translate(-45.000000, -45.000000)" stroke-width="14" stroke="'+color+'"><g id="bg"><g id="animated" transform="translate(10.000000, 33.000000)"><circle id="Oval-22" cx="68" cy="45" r="26"></circle></g></g></g></g></svg>']
+			var shapes       = ['<svg width="34px" height="31px" viewBox="0 0 34 31" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">    <!-- Generator: Sketch 43.2 (39069) - http://www.bohemiancoding.com/sketch -->    <desc>Created with Sketch.</desc>    <defs></defs>    <g id="design-v2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">        <g id="iPhone-7-Copy-68" transform="translate(-172.000000, -33.000000)" class="place-color">            <g id="bg">                <g id="animated" transform="translate(10.000000, 33.000000)">                    <polygon id="Polygon" points="179 0 195.167961 11.7467111 188.992349 30.7532889 169.007651 30.7532889 162.832039 11.7467111"></polygon>                </g></g></g></g></svg>', '<svg width="62px" height="64px" viewBox="0 0 62 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><!-- Generator: Sketch 43.2 (39069) - http://www.bohemiancoding.com/sketch --><desc>Created with Sketch.</desc><defs></defs><g id="design-v2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="iPhone-7-Copy-68" transform="translate(-34.000000, -162.000000)" stroke-width="8" class="place-stroke"><g id="bg"><g id="animated" transform="translate(10.000000, 33.000000)"><path d="M43,134.408029 L69.6816418,176 L16.3183582,176 L43,134.408029 Z" id="Triangle-2" transform="translate(43.000000, 153.500000) rotate(-27.000000) translate(-43.000000, -153.500000) "></path></g></g></g></g></svg>', '<svg width="66px" height="66px" viewBox="0 0 66 66" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><!-- Generator: Sketch 43.2 (39069) - http://www.bohemiancoding.com/sketch --><desc>Created with Sketch.</desc><defs></defs><g id="design-v2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="iPhone-7-Copy-68" transform="translate(-45.000000, -45.000000)" stroke-width="14" class="place-stroke" ><g id="bg"><g id="animated" transform="translate(10.000000, 33.000000)"><circle id="Oval-22" cx="68" cy="45" r="26"></circle></g></g></g></g></svg>']
 			var item         = document.createElement('div');
 
 			item.innerHTML   = shapes[Math.floor(Math.random()*shapes.length)];;
@@ -25,7 +25,14 @@ var animate = {
 			item.className   += "animate-shape animate-shape-in ";
 			svg.style.width  = size + 'px';
 			svg.style.height = size + 'px';
-
+			var fill = svg.querySelector('.place-color');
+			var stroke = svg.querySelector('.place-stroke');
+			if(fill) {
+				fill.style.fill = color;
+			} else {
+				stroke.style.fill = color;
+			}
+		
 			// svg.setAttribute('transform', 'rotate('+Math.floor(Math.random() * 180) + 5 +')');
 
 			randomLocation(item);
@@ -56,10 +63,9 @@ var animate = {
 
 	},
 	loginTransition: function () {
-		var animateWrapper = document.querySelector('.fn-animate-wrapper');
 
 		window.setTimeout(function () {
-			animateWrapper.setAttribute('splash', 'finished');
+			body.setAttribute('splash', 'finished');
 		}, 200)
 	},
 	login: function () {
--- src/javascript/motionController.js
+++ src/javascript/motionController.js
@@ -1,47 +1,47 @@
-var motion = {
-	first:true,
-	minDiff: 30,
-	base:0,
-	init: function () {
-		motion.right();
-		// motion.left();
-		console.log('motion.init');
-	},
-	right: function () {
-		window.addEventListener('deviceorientation', motion.motionEvent);
-	},
-	motionEvent: function (event, re) {
-		if(motion.first) {
-			motion.base  = event.alpha;        
-			motion.first = false;              
-		}
-		var change = event.alpha - motion.base;
+// var motion = {
+// 	first:true,
+// 	minDiff: 30,
+// 	base:0,
+// 	init: function () {
+// 		motion.right();
+// 		// motion.left();
+// 		console.log('motion.init');
+// 	},
+// 	right: function () {
+// 		window.addEventListener('deviceorientation', motion.motionEvent);
+// 	},
+// 	motionEvent: function (event, re) {
+// 		if(motion.first) {
+// 			motion.base  = event.alpha;        
+// 			motion.first = false;              
+// 		}
+// 		var change = event.alpha - motion.base;
 
-		if(change *-1 > motion.minDiff) {
-			console.log('got it!');
-			motion.changeLive();
-			window.removeEventListener('deviceorientation', motion.motionEvent);
+// 		if(change *-1 > motion.minDiff) {
+// 			console.log('got it!');
+// 			motion.changeLive();
+// 			window.removeEventListener('deviceorientation', motion.motionEvent);
 			
 			
 
-		}
-	},
-	changeLive: function () {
-		$.post({
-		 url: '/play/live',
-		 data: {
-		 	isLive: true
-		 },
-		 success: function(res){
-		 	console.log('success', res);
-		 	window.reload();
-		  // window.location = res.redirectTo;
-		 },
-		 error: function (res) {
-		 	console.log('error', res);
-		 	// alert('too much')
-		 	// window.location = res.redirectTo;
-		 }
-		});
-	}
-}
+// 		}
+// 	},
+// 	changeLive: function () {
+// 		$.post({
+// 		 url: '/play/live',
+// 		 data: {
+// 		 	isLive: true
+// 		 },
+// 		 success: function(res){
+// 		 	console.log('success', res);
+// 		 	window.reload();
+// 		  // window.location = res.redirectTo;
+// 		 },
+// 		 error: function (res) {
+// 		 	console.log('error', res);
+// 		 	// alert('too much')
+// 		 	// window.location = res.redirectTo;
+// 		 }
+// 		});
+// 	}
+// }
